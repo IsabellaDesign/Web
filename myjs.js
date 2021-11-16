1efdf6fc5b8db215d6f08a319b910e51dd78ab36
Margarita Madzharova <marg5405@stud.kea.dk>
2021-11-16 13:32:58 +0100

    firstpushmarg

--- myjs.js
+++ myjs.js
@@ -23,3 +23,28 @@ function setupBags(bagArray) {
     document.querySelector("main").appendChild(copy);
   });
 }
+var menu = document.getElementById("menu");
+var nav = document.getElementById("navigation");
+var exit = document.getElementById("exit");
+
+menu.addEventListener("click", function (e) {
+  nav.classList.add("nav-transition");
+  e.preventDefault();
+});
+
+exit.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+  e.preventDefault();
+});
+
+nav.addEventListener("click", function (e) {
+  nav.classList.remove("nav-transition");
+});
+
+function openNav() {
+  document.getElementById("slideNav").style.width = "1000px";
+}
+
+function closeNav() {
+  document.getElementById("slideNav").style.width = "0";
+}
