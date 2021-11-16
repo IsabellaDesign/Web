2826fb25df52e03792f4ab533820f8e1a7d865f3
Arina Afanasjeva <arin0181@stud.kea.dk>
2021-11-16 11:27:00 +0100

    images

--- myjs.js
+++ myjs.js
@@ -1,25 +1,43 @@
 getTheBags();
+getTheImages();
 
-function getTheBags() {
-  fetch(
-    "http://arimmdna.eu/kea/2/digitalcontent/wp21S/wp-json/wp/v2/bag?per-page=100&_embed"
-  )
+function getTheImages() {
+  fetch("http://arimmdna.eu/kea/2/digitalcontent/wp21S/wp-json/wp/v2/media")
     .then((res) => res.json())
-    .then(setupBags);
+    .then(setupImages);
 }
 
-function setupBags(bagArray) {
-  console.log(bagArray);
+function setupImages(imgArray) {
   const template = document.querySelector("template#template").content;
-  const parentElement = document.querySelector("main");
+  const parentElement = document.querySelector(".collectionSection");
 
-  bagArray.forEach((bag) => {
+  imgArray.forEach((bag) => {
     const copy = template.cloneNode(true);
-    copy.querySelector("h1.bagName").textContent = bag.title.rendered;
-
-    // copy.querySelector("img.bagPic").src =
-    //   bag._embedded["wp:featuremedia"][0].media_details.sizes.full.source_url;
-
+    copy.querySelector("img").src = bag.guid.rendered;
     document.querySelector("main").appendChild(copy);
   });
 }
+
+// function getTheBags() {
+//   fetch(
+//     "http://arimmdna.eu/kea/2/digitalcontent/wp21S/wp-json/wp/v2/bag?per-page=100&_embed"
+//   )
+//     .then((res) => res.json())
+//     .then(setupBags);
+// }
+
+// function setupBags(bagArray) {
+//   console.log(bagArray);
+//   const template = document.querySelector("template#template").content;
+//   const parentElement = document.querySelector(".collectionSection");
+
+//   bagArray.forEach((bag) => {
+//     const copy = template.cloneNode(true);
+//     copy.querySelector("h1.bagName").textContent = bag.title.rendered;
+
+//     // copy.querySelector("img.bagPic").src =
+//     //   bag._embedded["wp:featuremedia"][0].media_details.sizes.full.source_url;
+
+//     document.querySelector("main").appendChild(copy);
+//   });
+// }
